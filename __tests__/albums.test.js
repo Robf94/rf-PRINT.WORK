be72c93c3b479f32a2b2658aed5d8b8d4d284b37
Rob Friskney <rfriskney94@gmail.com>
2024-12-01 14:58:23 +0000

    reverted sample test

--- __tests__/albums.test.js
+++ __tests__/albums.test.js
@@ -1,13 +1,17 @@
-const request = require("supertest")
-const app = require("../app")
+// const request = require("supertest")
+// const app = require("../app")
 
-describe("GET /api/albums", () => {
-  it('200: should respond with an array of the top 100 albums', () => {
-    return request(app)
-      .get("/api/albums")
-      .expect(200)
-      .then(({ body }) => {
-      console.log(body);
-    })
-  });
-})
+test("Sample test", () => {
+  expect(true).toBe(true);
+});
+
+// describe("GET /api/albums", () => {
+//   it('200: should respond with an array of the top 100 albums', () => {
+//     return request(app)
+//       .get("/api/albums")
+//       .expect(200)
+//       .then(({ body }) => {
+//       console.log(body);
+//     })
+//   });
+// })
